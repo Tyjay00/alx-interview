c67f731e4868d7f814602a8198dfdcf66e1c1a23
Tyjay00 <tyronejoel04@gmail.com>
2024-04-18 21:41:34 +0000

    0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,36 @@
 #!/usr/bin/node
+// Script that prints all characters of a Star Wars movie
 
 const request = require('request');
-const filmUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-request(filmUrl, (error, response, body) => {
-  if (error) {
-    return;
-  }
-  const film = JSON.parse(body);
-  const urlToName = {};
-  film.characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        return;
-      }
-      const character = JSON.parse(body);
-      urlToName[character.url] = character.name;
-      if (Object.keys(urlToName).length === film.characters.length) {
-        film.characters.forEach(characterUrl =>
-          console.log(urlToName[characterUrl]));
+const movieId = process.argv[2];
+const filmsApiUrl = 'https://swapi-api.hbtn.io/api/films';
+
+function fetchJSON (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(error);
       }
     });
   });
-});
+}
+
+async function printStarWarsCharacters (movieId) {
+  try {
+    const filmData = await fetchJSON(`${filmsApiUrl}/${movieId}`);
+    for (const characterUrl of filmData.characters) {
+      try {
+        const characterData = await fetchJSON(characterUrl);
+        console.log(characterData.name);
+      } catch (error) {
+        console.error(`Error fetching character data: ${error}`);
+      }
+    }
+  } catch (error) {
+    console.error(`Error fetching movie data: ${error}`);
+  }
+}
+
+printStarWarsCharacters(movieId);
